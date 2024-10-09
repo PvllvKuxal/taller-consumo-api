cf8e47217eb8ef359b0e54fe8b70f9e72566090c
PvllvKuxal <j.contreras26@ufromail.cl>
2024-10-09 15:56:13 -0300

    Logro(api): se sube el taller de consumo de api
    +El proyecto esta creado con vue y usa las 4 api instanciadas en el taller
    +se alcanzo a mostrar información en párrafos de una api

---
+++ taller_api/README.md
@@ -0,0 +1,26 @@
+# taller_api
+Las solicitudes hacia la api se aprecian en la consola , obteniendo datos y almacenandolos en variables para
+poder mostrar informacion relevante de las api (lamentablemente solo pude aplicar el consejo de el ayudante con la primera api que me funciono)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ taller_api/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <table>
+      <ul>
+        <button @click="obtenerPoblacion()">Obtener Población de Países</button>
+        <li v-for="item in poblacion" :key="item.country">{{ item.country }}: {{ item.populationCounts[0].value }}</li>
+      </ul>
+      <ul>
+        <button @click="obtenerRandomAnime()">Obtener anime random</button>
+        <!--v-for="item in randomAnime" :key=""-->
+        <!-- No alcanceeeeee :,) -->
+      </ul>
+      <ul>
+        <button @click="obtenerRandomManga()">Obtener manga random</button>
+      </ul>
+      <ul>
+        <button @click="obtenerRecomendaciones()">Obtener recomendaciones Anime</button>
+      </ul>
+    </table>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      poblacion: [],
+      randomAnime: [],
+      randomManga: [],
+      recomendacionesAnime: []
+    };
+  },
+  methods: {
+    obtenerPoblacion() {
+      fetch('https://countriesnow.space/api/v0.1/countries/population')
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          this.poblacion = data.data;
+          console.log(this.poblacion)
+        })
+        .catch(error => {
+          console.error('Error al obtener los datos:', error);
+        });
+    },
+    obtenerRandomAnime(){
+      fetch('https://api.jikan.moe/v4/random/anime')
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          this.randomAnime = data.data;
+          console.log(this.randomAnime)
+        })
+        .catch(error => {
+          console.error('Error al obtener los datos:', error);
+        });
+    },
+    obtenerRandomManga(){
+      fetch('https://api.jikan.moe/v4/random/manga')
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          this.randomManga = data.data;
+          console.log(this.randomManga)
+        })
+        .catch(error => {
+          console.error('Error al obtener los datos:', error);
+        });
+    },
+    obtenerRecomendaciones(){
+      fetch('https://api.jikan.moe/v4/recommendations/anime')
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          this.recomendacionesAnime = data.data;
+          console.log(this.recomendacionesAnime)
+        })
+        .catch(error => {
+          console.error('Error al obtener los datos:', error);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+</style>
